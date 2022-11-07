c24606f0e47649f2b241ddaaf8d08bed1607631c
Yubin <ryzond6728@gmail.com>
2022-11-07 13:30:07 +0900

    Add granteeAddresses

--- src/constants/grantableChains.ts
+++ src/constants/grantableChains.ts
@@ -1,20 +1,3 @@
-import { CHAIN_NAMES } from "./embedChainInfos";
+import { GRANTEE_BECH32_ADDRESSES } from "./granteeAddresses";
 
-export const GRANTABLE_CHAINS: string[] = [
-  CHAIN_NAMES.Osmosis,
-  CHAIN_NAMES.CosmosHub,
-  CHAIN_NAMES.Juno,
-  CHAIN_NAMES.Evmos,
-  CHAIN_NAMES.Persistence,
-  CHAIN_NAMES.Stargaze,
-  CHAIN_NAMES.Secret,
-  CHAIN_NAMES.SifChain,
-  CHAIN_NAMES.Kava,
-  CHAIN_NAMES.Sentinel,
-  CHAIN_NAMES.Akash,
-  CHAIN_NAMES.CRO,
-  CHAIN_NAMES.Gravity,
-  CHAIN_NAMES.Umee,
-  CHAIN_NAMES.Regen,
-  CHAIN_NAMES.Agoric,
-];
+export const GRANTABLE_CHAINS = Object.keys(GRANTEE_BECH32_ADDRESSES);
---
+++ src/constants/granteeAddresses.ts
@@ -0,0 +1,21 @@
+import { CHAIN_NAMES } from "./embedChainInfos";
+
+export const GRANTEE_BECH32_ADDRESSES = {
+  [CHAIN_NAMES.Osmosis]: "osmo1wuy97alha699xlp0dv4z6y3fp30slchenudupt",
+  [CHAIN_NAMES.CosmosHub]: "cosmos1wuy97alha699xlp0dv4z6y3fp30slchem87vhe",
+  [CHAIN_NAMES.Juno]: "juno1wuy97alha699xlp0dv4z6y3fp30slched4ahs9",
+  [CHAIN_NAMES.Evmos]: "evmos1pq4xyvn23xvaasr3lts92taleurj7wwpcprstx",
+  [CHAIN_NAMES.Persistence]:
+    "persistence1wuy97alha699xlp0dv4z6y3fp30slche4tclea",
+  [CHAIN_NAMES.Stargaze]: "stars1wuy97alha699xlp0dv4z6y3fp30slche0mf3ug",
+  [CHAIN_NAMES.Secret]: "secret1ap9g3x5aty2dfrdsd7hdhq8rtfe9pr0jc5r2cg",
+  [CHAIN_NAMES.SifChain]: "sif1wuy97alha699xlp0dv4z6y3fp30slche7636cj",
+  [CHAIN_NAMES.Kava]: "kava1w75sqftl3zm27lqpf765p6zdj4n0lfz8g48p4c",
+  [CHAIN_NAMES.Sentinel]: "sent1wuy97alha699xlp0dv4z6y3fp30slchequg4nk",
+  [CHAIN_NAMES.Akash]: "akash1wuy97alha699xlp0dv4z6y3fp30slchekuntwr",
+  [CHAIN_NAMES.CRO]: "cro147jmdckwq5ua8g7vccm80vjej5vvmkvvjfhr7v",
+  [CHAIN_NAMES.Gravity]: "gravity1wuy97alha699xlp0dv4z6y3fp30slchelhv5j3",
+  [CHAIN_NAMES.Umee]: "umee1wuy97alha699xlp0dv4z6y3fp30slchef3rnnt",
+  [CHAIN_NAMES.Regen]: "regen1wuy97alha699xlp0dv4z6y3fp30slchey94spa",
+  [CHAIN_NAMES.Agoric]: "agoric1gwrusxqjcer56ex7f9umdtlas2v9apznk8j30r",
+};
